1c03d72f1a433c9debeec6b70e8231caf463418b
Akashii0 <abdulkid151@gmail.com>
2025-01-18 11:46:57 +0100

    Data Types

---
+++ main.py
@@ -0,0 +1,45 @@
+# DataTypes 
+
+"""
+Strings
+Integers
+Booleans
+Lists
+Tuples
+Dictionaries
+"""
+
+
+# Strings
+"Paul"
+
+# Integers
+1234567890
+
+# Booleans
+True
+False
+
+# Lists
+[1, 2, 3, 4, 5]
+names = ["Paul", "John", "George", "Ringo"]
+
+# Tuples
+tupleNames = (1, 2, 3, 4, 5)
+
+# Dictionaries
+{
+    "name": "Paul",
+    "age": 27
+}
+
+{
+    "name": "Adeola",
+}
+
+
+tupleNames.append("akeem")
+names.append("Akeem")
+print(names)
+print(tupleNames)
+
