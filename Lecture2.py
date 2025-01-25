b8aab908095e5aede995923fe7388d221dfb6ce1
Akashii0 <abdulkid151@gmail.com>
2025-01-25 11:18:28 +0100

    Updated code

--- Lecture2.py
+++ Lecture2.py
@@ -2,3 +2,28 @@
 
 # Classes are blueprint for creating Objects
 # Objects they bundle Attributes and Methods (Functions)
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Welcome to the GDG Bootcamp {self.name}, you are {self.age} years old")  # noqa
+
+
+class Student(Person):
+    def __init__(self, name, age, Dept):
+        super().__init__(name, age)
+        self.Dept = Dept
+
+    def greet(self):
+        print(f"Heyyyy {self.name} from {self.Dept}, You are {self.age} years old!!")  # noqa
+
+
+person1 = Person("Paul", "25")
+person2 = Person("Akeem", "19")
+student1 = Student("Akeem", "19", "Electrical and Electronics")
+person1.greet()
+person2.greet()
+student1.greet()
