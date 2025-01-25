1a9c0830f0b3c0dc32004884b5a790157530e906
Akashii0 <abdulkid151@gmail.com>
2025-01-25 10:50:27 +0100

    Updated lecture2

--- Lecture2.py
+++ Lecture2.py
@@ -1,12 +1,15 @@
 # Classes
 
+# Classes are blueprint for creating Objects
+# Objects they bundle Attributes and Methods (Functions)
+
 class Person():
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def greet(self):
-        print(f"Heyyyy welcome to the Bootcamp! {self.name}, You are {self.age} years old")  # noqa
+        print(f"Welcome to the GDG Bootcamp {self.name}, you are {self.age} years old")  # noqa
 
 
 class Student(Person):
@@ -15,10 +18,12 @@ class Student(Person):
         self.Dept = Dept
 
     def greet(self):
-        print(f"Heyyyy welcome to the Bootcamp! {self.name} from {self.Dept}")
+        print(f"Heyyyy {self.name} from {self.Dept}, You are {self.age} years old!!")  # noqa
 
 
 person1 = Person("Paul", "25")
-student1 = Student("Paul", "25", "Mechatronics")  # noqa
+person2 = Person("Akeem", "19")
+student1 = Student("Akeem", "19", "Electrical and Electronics")
 person1.greet()
+person2.greet()
 student1.greet()
